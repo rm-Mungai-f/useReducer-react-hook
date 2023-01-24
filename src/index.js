f27a32958526fc5356d48a0653d957274061b9bd
rm-Mungai-f <mungai.mbugua1409@gmail.com>
2023-01-24 12:24:04 +0300

    demonstration of a useReducer react hook

--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useReducer } from "react";
+import ReactDOM from "react-dom";
+//import profile1 from './image/profile.jpg';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const App = () =>{
+//     return(
+//         <div className="ui comments">
+//             <div className="comment">
+//                 <a href="/" className="avatar">
+//                     <img src={profile1} alt="profile"/>
+//                 </a>
+//                 <div className="content">
+//                     <a href="/" className="author">
+//                         Sarah
+//                     </a>
+//                 </div>
+//                 <div className="metadata">
+//                     <span className="date">
+//                         Today at 11:00AM
+//                     </span>
+//                 </div>
+//                 <div className="text">
+//                     It's amazing
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//use Reducer hook
+//Reducer is a pure function that takes state and action as arguments and returns a new state
+
+//syntax
+/* const [state,dispatch]=useReducer(reducer,initialState)*/
+const initialState={
+    total:0,
+    totalIncrement:0,
+    totalDecrement:0
+}
+
+//reducer to return new state
+const reducer=(state,action)=>{
+    switch(action.type){
+        case "INCREMENT":
+            return{
+                ...state,
+                total:state.total+1,
+                totalIncrement:state.totalIncrement+1
+            };
+        case "DECREMENT":
+            return{
+                ...state,
+                total:state.total-1,
+                totalDecrement:state.totalDecrement-1
+            };
+    }
+}
+
+const Counter=()=>{
+    const[values,dispatch]=useReducer(reducer,initialState);
+    return(
+        <div>
+            <button onClick={()=>dispatch({type:"INCREMENT"})}>Increment</button>
+            <button onClick={()=>dispatch({type:"DECREMENT"})}>Decrement</button>
+            <hr/>
+            <h3>Total:{values.total}</h3>
+            <h3>Total increment:{values.totalIncrement}</h3>
+            <h3>Total decrement:{values.totalDecrement}</h3>
+        </div>
+    );
+};
+export default Counter;
+
+
+// ReactDOM.render(
+//     <App />,
+//     document.querySelector('#root')
+// )
+ReactDOM.render(
+    <Counter />,
+    document.querySelector('#root')
+)
